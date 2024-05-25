9b63b5f3621041a7a15faf3f2847551fa4531ac9
komangchandra <komangchandraaa1@gmail.com>
2024-05-25 14:10:13 +0700

    feat: API rekap kehadiran dokter

---
+++ migrations/20240525023829-create-rekap-hadir-dokter.js
@@ -0,0 +1,70 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("rekap_hadir_dokter", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      tanggal: {
+        type: Sequelize.STRING,
+      },
+      bulan: {
+        type: Sequelize.STRING,
+      },
+      tahun: {
+        type: Sequelize.STRING,
+      },
+      nama_shift: {
+        type: Sequelize.STRING,
+      },
+      nama_dokter: {
+        type: Sequelize.STRING,
+      },
+      jbtn: {
+        type: Sequelize.STRING,
+      },
+      nominal_shift: {
+        type: Sequelize.INTEGER,
+      },
+      garansi_fee: {
+        type: Sequelize.INTEGER,
+      },
+      barcode: {
+        type: Sequelize.STRING,
+      },
+      denda_telat: {
+        type: Sequelize.INTEGER,
+      },
+      nama_dokter_pengganti: {
+        type: Sequelize.STRING,
+      },
+      telat: {
+        type: Sequelize.INTEGER,
+      },
+      nominal: {
+        type: Sequelize.INTEGER,
+      },
+      total: {
+        type: Sequelize.INTEGER,
+      },
+      kekurangan_garansi_fee: {
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("rekap_hadir_dokter");
+  },
+};
--- routes/rekapKehadiran/dokter.js
+++ routes/rekapKehadiran/dokter.js
@@ -3,41 +3,150 @@ const connection = require("../../config/database");
 
 const router = express.Router();
 
-// Ambil semua data
-router.get("/", function (req, res, next) {
-  const stringQuery = `SELECT k.*, p.nama, p.jbtn, p.jk, dj.tanggal, dj.isTelat, dj.isPulangCepat, dj.nominal
-  FROM kehadiran k
-  JOIN barcode b ON k.barcode = b.barcode
-  JOIN pegawai p ON b.id = p.id
-  JOIN detail_jadwal dj ON k.id_detail_jadwal = dj.id
-  WHERE p.jbtn = 'dr'
-  AND k.foto_keluar IS NOT NULL;
-`;
+// Ambil semua data dari tabel kehadiran
+router.post("/cek", async function (req, res, next) {
+  const { bulan, tahun } = req.body;
 
-  connection.query(stringQuery, (error, result) => {
-    if (error) {
-      console.log("Error executing query", error);
-      return;
-    }
-    console.log("Sukses ");
+  if (!bulan || !tahun) {
+    res.status(400).send("Bulan dan tahun harus disertakan.");
+    return;
+  }
+
+  const stringQuery = `SELECT * FROM rekap_hadir_dokter WHERE bulan = ? AND tahun = ?`;
+
+  try {
+    const result = await new Promise((resolve, reject) => {
+      connection.query(stringQuery, [bulan, tahun], (error, result) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        resolve(result);
+      });
+    });
+
+    console.log("Sukses");
     res.json(result);
-  });
+  } catch (error) {
+    console.log("Error executing query", error);
+    res.status(500).send("Error executing query");
+  }
 });
 
-// ambil data berdasarkan id
-router.get("/:id_shift", function (req, res, next) {
-  const { id_shift } = req.params;
+router.post("/delete", async function (req, res, next) {
+  const { bulan, tahun } = req.body;
 
-  const showQuery = `SELECT * FROM shift WHERE id_shift = ` + id_shift;
+  if (!bulan || !tahun) {
+    res.status(400).send("Bulan dan tahun harus disertakan.");
+    return;
+  }
 
-  connection.query(showQuery, (error, result) => {
-    if (error) {
-      console.log("Error executing query", error);
-      return;
-    }
-    console.log("Query result:", result);
+  const stringQuery = `DELETE FROM rekap_hadir_dokter WHERE bulan = ? AND tahun = ?`;
+
+  try {
+    const result = await new Promise((resolve, reject) => {
+      connection.query(stringQuery, [bulan, tahun], (error, result) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        resolve(result);
+      });
+    });
+
+    console.log("Sukses");
+    res.json({ message: "Data berhasil dihapus" });
+  } catch (error) {
+    console.log("Error executing query", error);
+    res.status(500).send("Error executing query");
+  }
+});
+
+router.post("/get", async function (req, res, next) {
+  const { bulan, tahun } = req.body;
+
+  if (!bulan || !tahun) {
+    res.status(400).send("Bulan dan tahun harus disertakan.");
+    return;
+  }
+
+  const stringQuery = `SELECT k.*, p.nama, p.jbtn, p.jk, dj.tanggal, jk.tanggal_awal, jk.tanggal_akhir, s.nama_shift, s.nominal, s.garansi_fee
+  FROM kehadiran k
+  JOIN detail_jadwal dj ON k.id_detail_jadwal = dj.id
+  JOIN jadwal_kehadiran jk ON k.id_jadwal = jk.id
+  JOIN barcode b ON k.barcode = b.barcode
+  JOIN pegawai p ON b.id = p.id
+  JOIN shift s ON k.id_shift = s.id_shift
+  WHERE p.jbtn = 'dr'
+    AND jk.bulan = ?
+    AND jk.tahun = ?
+    AND k.foto_keluar IS NOT NULL;`;
+
+  try {
+    const result = await new Promise((resolve, reject) => {
+      connection.query(stringQuery, [bulan, tahun], (error, result) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        resolve(result);
+      });
+    });
+
+    console.log("Sukses");
+
+    // Iterasi melalui setiap item dalam hasil query dan lakukan operasi insert
+    await Promise.all(
+      result.map(async (item) => {
+        const insertQuery = `INSERT INTO rekap_hadir_dokter 
+  (tanggal, bulan, tahun, nama_shift, nama_dokter, jbtn, nominal_shift, garansi_fee, barcode, denda_telat, nama_dokter_pengganti, telat, nominal, total, kekurangan_garansi_fee, createdAt, updatedAt)
+  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+
+        const currentDate = new Date();
+        const values = [
+          item.tanggal,
+          bulan,
+          tahun,
+          item.nama_shift,
+          item.nama,
+          item.jbtn,
+          item.nominal,
+          item.garansi_fee,
+          item.barcode,
+          item.denda_telat,
+          item.nama_dokter_pengganti,
+          item.telat,
+          item.nominal,
+          (total = item.nominal - item.denda_telat),
+          (kekurangan_garansi_fee = item.garansi_fee - total),
+          currentDate,
+          currentDate,
+        ];
+
+        try {
+          await new Promise((resolve, reject) => {
+            connection.query(insertQuery, values, (error, results) => {
+              if (error) {
+                reject(error);
+              } else {
+                console.log(
+                  "Data berhasil disisipkan ke tabel rekap_hadir_dokter"
+                );
+                resolve();
+              }
+            });
+          });
+        } catch (error) {
+          console.log("Error executing insert query", error);
+        }
+      })
+    );
+
     res.json(result);
-  });
+  } catch (error) {
+    console.log("Error executing query", error);
+    res.status(500).send("Error executing query");
+  }
 });
 
 module.exports = router;
